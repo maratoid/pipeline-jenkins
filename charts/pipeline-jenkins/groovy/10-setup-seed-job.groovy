0b2cf8c361196662085cca96090dd6ec3a924583
Marat G <maratoid@gmail.com>
2018-03-07 11:37:00 -0800

    add running seed jobdsl job, add vault plugin and config

---
+++ charts/pipeline-jenkins/groovy/10-setup-seed-job.groovy
@@ -0,0 +1,33 @@
+import jenkins.model.Jenkins.*
+import javaposse.jobdsl.dsl.*
+import javaposse.jobdsl.plugin.*
+
+JenkinsJobManagement jm = new JenkinsJobManagement(System.out, [:], new File('.'));
+DslScriptLoader dslScriptLoader = new DslScriptLoader(jm)
+dslScriptLoader.runScript("""
+job('{{ .Values.github.jobsRepo }}-jobs') {
+
+  displayName('Re-create {{ .Values.github.jobsRepo }} jobs')
+  description('Re-create all jobs from the {{ .Values.github.jobsRepo }} repository')
+
+  blockOn('.*-pipeline') {
+    blockLevel('GLOBAL')
+    scanQueueFor('ALL')
+  }
+
+  scm {
+    github('samsung-cnct/pipeline-jobs', 'master', 'https', 'github.com')
+  }
+
+  steps {
+    dsl {
+      external 'jobdsl/**/*.groovy'
+      additionalClasspath(['lib/*.jar','src/main/groovy'].join('\\n'))
+      removeAction 'DELETE'
+    }
+  }
+}
+""")
+
+def job = Jenkins.instance.getItem('{{ .Values.github.jobsRepo }}-jobs')
+job.scheduleBuild()
